c251b7d8f7755822be5c50b821b68db92aebd23c
Bárbara Romero <bqromero@gmail.com>
2021-11-26 11:19:37 -0300

    Criar model para gravadora

---
+++ src/main/java/br/com/zup/ZupFy/gravadora/Gravadora.java
@@ -0,0 +1,35 @@
+package br.com.zup.ZupFy.gravadora;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "gravadoras")
+public class Gravadora {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  private String nome;
+
+  public Gravadora() {
+
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+}
