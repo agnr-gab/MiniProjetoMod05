72aa17a81ec6a8bcecb4fd1895610636a9e1aae6
Bárbara Romero <bqromero@gmail.com>
2021-11-29 10:46:24 -0300

    Adicionar injeções de dependência

--- src/main/java/br/com/zup/ZupFy/artista/ArtistaService.java
+++ src/main/java/br/com/zup/ZupFy/artista/ArtistaService.java
@@ -1,13 +1,24 @@
 package br.com.zup.ZupFy.artista;
 
 
+import br.com.zup.ZupFy.album.AlbumRepository;
+import br.com.zup.ZupFy.gravadora.GravadoraRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ArtistaService {
+  private ArtistaRepository artistaRepository;
+  private GravadoraRepository gravadoraRepository;
+  private AlbumRepository albumRepository;
+
 
   @Autowired
-  private ArtistaRepository artistaRepository;
+  public ArtistaService(ArtistaRepository artistaRepository, GravadoraRepository gravadoraRepository,
+                        AlbumRepository albumRepository) {
+    this.artistaRepository = artistaRepository;
+    this.gravadoraRepository = gravadoraRepository;
+    this.albumRepository = albumRepository;
+  }
 
 }
