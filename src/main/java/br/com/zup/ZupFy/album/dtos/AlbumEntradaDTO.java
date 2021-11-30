2a907363a5e0d0311467194a96cd67abf1dc3749
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-30 16:15:41 -0300

    Criar metodo Buscar Album Por Id

---
+++ src/main/java/br/com/zup/ZupFy/album/dtos/AlbumEntradaDTO.java
@@ -0,0 +1,2 @@
+package br.com.zup.ZupFy.album.dtos;public class AlbumEntradaDTO {
+}
--- src/main/java/br/com/zup/ZupFy/artista/ArtistaService.java
+++ src/main/java/br/com/zup/ZupFy/artista/ArtistaService.java
@@ -1,49 +1,62 @@
 package br.com.zup.ZupFy.artista;
 
 
+import br.com.zup.ZupFy.album.Album;
 import br.com.zup.ZupFy.album.AlbumRepository;
+import br.com.zup.ZupFy.album.dtos.AlbumEntradaDTO;
 import br.com.zup.ZupFy.gravadora.GravadoraRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ArtistaService {
-  private ArtistaRepository artistaRepository;
-  private GravadoraRepository gravadoraRepository;
-  private AlbumRepository albumRepository;
+    private ArtistaRepository artistaRepository;
+    private GravadoraRepository gravadoraRepository;
+    private AlbumRepository albumRepository;
 
 
-  @Autowired
-  public ArtistaService(ArtistaRepository artistaRepository, GravadoraRepository gravadoraRepository,
-                        AlbumRepository albumRepository) {
-    this.artistaRepository = artistaRepository;
-    this.gravadoraRepository = gravadoraRepository;
-    this.albumRepository = albumRepository;
-  }
+    @Autowired
+    public ArtistaService(ArtistaRepository artistaRepository, GravadoraRepository gravadoraRepository,
+                          AlbumRepository albumRepository) {
+        this.artistaRepository = artistaRepository;
+        this.gravadoraRepository = gravadoraRepository;
+        this.albumRepository = albumRepository;
+    }
 
-  public Artista salvarArtista(Artista artista) {
-    artistaRepository.save(artista);
-    return artista;
-  }
+    public Artista salvarArtista(Artista artista) {
+        artistaRepository.save(artista);
+        return artista;
+    }
 
-  public Artista verificarArtista(Artista artista) {
-    if (artistaRepository.existsByNome(artista.getNome())) {
-      throw new RuntimeException();
+    public Artista verificarArtista(Artista artista) {
+        if (artistaRepository.existsByNome(artista.getNome())) {
+            throw new RuntimeException();
+
+        }
+        return salvarArtista(artista);
+    }
+
+    public List<Artista> exibirLista() {
+        List<Artista> listaDeArtistas = (List<Artista>) artistaRepository.findAll();
+        return listaDeArtistas;
+    }
+
+    public List<Artista> buscarPorNome(String nome) {
+        return artistaRepository.findAllByNome(nome);
 
     }
-    return salvarArtista(artista);
-  }
 
-  public List<Artista> exibirLista() {
-    List<Artista> listaDeArtistas = (List<Artista>) artistaRepository.findAll();
-    return listaDeArtistas;
-  }
+    public Album buscarPorId(Integer id) {
+        Optional<Album> album = albumRepository.findById(id);
+        //TODO: fazer a exceção
+        if (album.isPresent()) {
 
-  public List<Artista> buscarPorNome(String nome) {
-    return artistaRepository.findAllByNome(nome);
-
-  }
+        }
+        return album.get();
+    }
 
 }
