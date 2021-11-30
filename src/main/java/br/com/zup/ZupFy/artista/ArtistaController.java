cdda163358192879404e0ccf40b60c9a577cd990
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-30 16:23:29 -0300

    Inserir metodo de atualizar albuns

--- src/main/java/br/com/zup/ZupFy/artista/ArtistaController.java
+++ src/main/java/br/com/zup/ZupFy/artista/ArtistaController.java
@@ -1,6 +1,8 @@
 package br.com.zup.ZupFy.artista;
 
 
+import br.com.zup.ZupFy.album.Album;
+import br.com.zup.ZupFy.album.dtos.AlbumEntradaDTO;
 import br.com.zup.ZupFy.artista.dtos.ArtistaEntradaDTO;
 import br.com.zup.ZupFy.artista.dtos.ArtistaResumoDTO;
 import br.com.zup.ZupFy.artista.dtos.ArtistaSaidaDTO;
@@ -16,41 +18,48 @@ import java.util.List;
 @RequestMapping("/artistas")
 public class ArtistaController {
 
-  @Autowired
-  private ArtistaService artistaService;
+    @Autowired
+    private ArtistaService artistaService;
 
-  @Autowired
-  private ModelMapper modelMapper;
+    @Autowired
+    private ModelMapper modelMapper;
 
-  @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
-  public ArtistaEntradaDTO cadastrarArtista(@RequestBody ArtistaEntradaDTO artistaEntradaDTO) {
-    Artista artista = modelMapper.map(artistaEntradaDTO, Artista.class);
-    return modelMapper.map(artistaService.verificarArtista(artista), ArtistaEntradaDTO.class);
-
-  }
-
-  @GetMapping
-  public List<ArtistaSaidaDTO> exibirArtista() {
-    List<ArtistaSaidaDTO> listaDeArtistas = new ArrayList<>();
-    for (Artista artista : artistaService.exibirLista()) {
-      ArtistaSaidaDTO artistaSaidaDTO = modelMapper.map(artista, ArtistaSaidaDTO.class);
-      listaDeArtistas.add(artistaSaidaDTO);
-    }
-    return listaDeArtistas;
-  }
-
-  @GetMapping("/{nome}")
-  public List<ArtistaResumoDTO> buscarAlbunsPorNome(@PathVariable String nome) {
-    List<ArtistaResumoDTO> artistaResumoDTOS = new ArrayList<>();
-    List<Artista> artistas = artistaService.buscarPorNome(nome);
-    for (Artista referencia : artistas) {
-      ArtistaResumoDTO conversao = modelMapper.map(referencia, ArtistaResumoDTO.class);
-      artistaResumoDTOS.add(conversao);
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ArtistaEntradaDTO cadastrarArtista(@RequestBody ArtistaEntradaDTO artistaEntradaDTO) {
+        Artista artista = modelMapper.map(artistaEntradaDTO, Artista.class);
+        return modelMapper.map(artistaService.verificarArtista(artista), ArtistaEntradaDTO.class);
 
     }
-    return artistaResumoDTOS;
 
-  }
+    @GetMapping
+    public List<ArtistaSaidaDTO> exibirArtista() {
+        List<ArtistaSaidaDTO> listaDeArtistas = new ArrayList<>();
+        for (Artista artista : artistaService.exibirLista()) {
+            ArtistaSaidaDTO artistaSaidaDTO = modelMapper.map(artista, ArtistaSaidaDTO.class);
+            listaDeArtistas.add(artistaSaidaDTO);
+        }
+        return listaDeArtistas;
+    }
 
+    @GetMapping("/{nome}")
+    public List<ArtistaResumoDTO> buscarAlbunsPorNome(@PathVariable String nome) {
+        List<ArtistaResumoDTO> artistaResumoDTOS = new ArrayList<>();
+        List<Artista> artistas = artistaService.buscarPorNome(nome);
+        for (Artista referencia : artistas) {
+            ArtistaResumoDTO conversao = modelMapper.map(referencia, ArtistaResumoDTO.class);
+            artistaResumoDTOS.add(conversao);
+
+        }
+        return artistaResumoDTOS;
+
+    }
+
+    @PutMapping("/{id}")
+    public AlbumEntradaDTO atualizarListaAlbuns(@PathVariable Integer id,
+                                                @RequestBody AlbumEntradaDTO albumEntradaDTO) {
+        //Album album = modelMapper.map(albumEntradaDTO, Album.class);
+
+        return modelMapper.map(artistaService.atualizarListaAlbum(id, albumEntradaDTO), AlbumEntradaDTO.class);
+    }
 }
