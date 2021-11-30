5e1b35ed17b91e7425f4f480ed639a9d4444c0cc
Bárbara Romero <bqromero@gmail.com>
2021-11-30 09:26:27 -0300

    Criar método para visualizar todos os artistas cadastrados

--- src/main/java/br/com/zup/ZupFy/artista/ArtistaController.java
+++ src/main/java/br/com/zup/ZupFy/artista/ArtistaController.java
@@ -2,10 +2,14 @@ package br.com.zup.ZupFy.artista;
 
 
 import br.com.zup.ZupFy.artista.dtos.ArtistaEntradaDTO;
+import br.com.zup.ZupFy.artista.dtos.ArtistaSaidaDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/artistas")
@@ -23,6 +27,16 @@ public class ArtistaController {
     Artista artista = modelMapper.map(artistaEntradaDTO, Artista.class);
     return modelMapper.map(artistaService.verificarArtista(artista), ArtistaEntradaDTO.class);
 
+  }
+
+  @GetMapping
+  public List<ArtistaSaidaDTO> exibirArtista() {
+    List<ArtistaSaidaDTO> listaDeArtistas = new ArrayList<>();
+    for (Artista artista : artistaService.exibirLista()) {
+      ArtistaSaidaDTO artistaSaidaDTO = modelMapper.map(artista, ArtistaSaidaDTO.class);
+      listaDeArtistas.add(artistaSaidaDTO);
+    }
+    return listaDeArtistas;
   }
 
 }
---
+++ src/main/java/br/com/zup/ZupFy/artista/dtos/ArtistaSaidaDTO.java
@@ -0,0 +1,31 @@
+package br.com.zup.ZupFy.artista.dtos;
+
+import br.com.zup.ZupFy.album.Album;
+
+import java.util.List;
+
+public class ArtistaSaidaDTO {
+
+  private String nome;
+  private List<Album> albuns;
+
+  public ArtistaSaidaDTO() {
+
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public List<Album> getAlbuns() {
+    return albuns;
+  }
+
+  public void setAlbuns(List<Album> albuns) {
+    this.albuns = albuns;
+  }
+}
