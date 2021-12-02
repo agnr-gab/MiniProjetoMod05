fdd7ed0c735ef7ec2b36ad667d3da9a87c58e29d
Bárbara Romero <bqromero@gmail.com>
2021-12-02 07:20:26 -0300

    Criar método que aplica filtro

--- src/main/java/br/com/zup/ZupFy/artista/ArtistaService.java
+++ src/main/java/br/com/zup/ZupFy/artista/ArtistaService.java
@@ -1,6 +1,7 @@
 package br.com.zup.ZupFy.artista;
 
 import br.com.zup.ZupFy.album.AlbumRepository;
+import br.com.zup.ZupFy.enums.Genero;
 import br.com.zup.ZupFy.gravadora.GravadoraRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,50 +11,57 @@ import java.util.Optional;
 
 @Service
 public class ArtistaService {
-    private ArtistaRepository artistaRepository;
-    private GravadoraRepository gravadoraRepository;
-    private AlbumRepository albumRepository;
+  private ArtistaRepository artistaRepository;
+  private GravadoraRepository gravadoraRepository;
+  private AlbumRepository albumRepository;
 
 
-    @Autowired
-    public ArtistaService(ArtistaRepository artistaRepository, GravadoraRepository gravadoraRepository,
-                          AlbumRepository albumRepository) {
-        this.artistaRepository = artistaRepository;
-        this.gravadoraRepository = gravadoraRepository;
-        this.albumRepository = albumRepository;
-    }
+  @Autowired
+  public ArtistaService(ArtistaRepository artistaRepository, GravadoraRepository gravadoraRepository,
+                        AlbumRepository albumRepository) {
+    this.artistaRepository = artistaRepository;
+    this.gravadoraRepository = gravadoraRepository;
+    this.albumRepository = albumRepository;
+  }
 
-    public Artista cadastrarArtista(Artista artista) {
-        Optional<Artista> optionalArtista = artistaRepository.findById(artista.getId());
-        if (optionalArtista.isPresent()) {
-            throw new RuntimeException();
-
-        }
-
-        return artistaRepository.save(artista);
-    }
-
-    public List<Artista> exibirListaArtistas() {
-        List<Artista> listaDeArtistas = (List<Artista>) artistaRepository.findAll();
-        return listaDeArtistas;
-    }
-
-    public Artista buscarPorId(Integer id) {
-        Optional<Artista> artista = artistaRepository.findById(id);
-        //TODO: fazer a exceção
-        if (artista.isPresent()) {
-
-        }
-        return artista.get();
-    }
-
-    public void deletarArtista(int id) {
-        if (artistaRepository.existsById(id)) {
-            artistaRepository.deleteById(id);
-        } else {
-            throw new RuntimeException();
-        }
+  public Artista cadastrarArtista(Artista artista) {
+    Optional<Artista> optionalArtista = artistaRepository.findById(artista.getId());
+    if (optionalArtista.isPresent()) {
+      throw new RuntimeException();
 
     }
+
+    return artistaRepository.save(artista);
+  }
+
+  public List<Artista> exibirListaArtistas() {
+    List<Artista> listaDeArtistas = (List<Artista>) artistaRepository.findAll();
+    return listaDeArtistas;
+  }
+
+  public Artista buscarPorId(Integer id) {
+    Optional<Artista> artista = artistaRepository.findById(id);
+    //TODO: fazer a exceção
+    if (artista.isPresent()) {
+
+    }
+    return artista.get();
+  }
+
+  public void deletarArtista(int id) {
+    if (artistaRepository.existsById(id)) {
+      artistaRepository.deleteById(id);
+    } else {
+      throw new RuntimeException();
+    }
+
+  }
+
+  public List<Artista> aplicarFiltro(Genero genero) {
+    if (genero != null) {
+      return artistaRepository.findAllByGenero(genero);
+    }
+    return exibirListaArtistas();
+  }
 
 }
